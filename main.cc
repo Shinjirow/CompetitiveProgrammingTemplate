f9c30c6e84cb2374cb9a043fdd10aab593bc13d3
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-06-08 10:30:41 +0900

    update

--- main.cc
+++ main.cc
@@ -19,6 +19,7 @@
 #include <stack>
 #include <regex>
 #include <tuple>
+#include <iomanip>
 
 #define int long long
 #define REP(i,a,n) for(int i=a;i<n;++i)
@@ -30,27 +31,30 @@
 #define mp make_pair
 #define fs first
 #define sc second
-#define mod 1000000007
 #define show(n) cerr<<#n<<" = "<<n<<endl
 #define showp(n) cerr<<n.fs<<", "<<n.sc<<endl
 #define shows(n) for(auto z:n){cerr<<z<<", ";}cerr<<endl
 #define showsp(n) for(auto z:n){cerr<<z.fs<<" "<<z.sc<<", "}cerr<<endl
 
-#define yes printf("Yes\n")
-#define no printf("No\n")
+#define yes puts("Yes")
+#define no puts("No")
 #define case(i) printf("Case #%lld: ",i)
 
 using namespace std;
 
-using ull=unsigned long long;
 using vi=vector<int>;
 using pint=pair<int,int>;
 
-const int INF = (1 << 30);
+inline void io(){cin.tie(0);ios::sync_with_stdio(false);cout.tie(0);cout<<fixed<<setprecision(20);}
+inline int  in(){int n;scanf("%lld",&n);return n;}
+inline void out(int n){printf("%lld\n",n);}
+
+const int INF=1LL<<55;
+const int MOD=1000000007;
+const double EPS=1e-8;
 
 signed main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
+    io();
 
     return 0;
 }
