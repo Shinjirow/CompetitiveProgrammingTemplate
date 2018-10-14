5a35ac3a8c7e3f1b7cd64cc9fb28015f0a09dcd6
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-10-14 15:52:28 +0900

    create resize multi-vector function, overload operators for std::vector, and update show() and out() function

--- main.cc
+++ main.cc
@@ -22,8 +22,8 @@
 #include <iomanip>
 
 #define int long long
-#define REP(i,l,r) REPEAT(i,l,r,true) //[l, r)
-#define rep(i,n) REP(i,0,n)           //[0, n)
+#define REP(i,l,r) REPEAT(i,l,r,true) // [l, r)
+#define rep(i,n) REP(i,0,n)           // [0, n)
 #define REPEAT(i,l,r,condition) for(int i=(condition)?l:r-1;(condition)?i<r:i>=l;(condition)?++i:--i) // false<-[l, r)->true
 #define all(e) e.begin(),e.end()
 #define rall(e) e.rbegin(),e.rend()
@@ -31,8 +31,7 @@
 #define fs first
 #define sc second
 #define show(...) cerr<<#__VA_ARGS__<<" = ";_DEBUG(__VA_ARGS__)
-#define shows(n) for(auto z:n){cerr<<z<<", ";}cerr<<endl
-#define showslr(n,l,r) cerr<<#n<<" = ";for(int i=l;i<r;i++){cerr<<n[i]<<", ";}cerr<<endl //[l, r)
+#define showlr(n,l,r) cerr<<#n<<" = ";for(int i=l;i<r;i++){cerr<<n[i]<<", ";}cerr<<endl // [l, r)
 
 #define yes puts("Yes")
 #define no puts("No")
@@ -45,13 +44,19 @@ using pint=pair<int,int>;
 
 inline void io(){cin.tie(0);ios::sync_with_stdio(false);cout.tie(0);cout<<fixed<<setprecision(20);}
 
-void _DEBUG(){cerr<<endl;}
-template<typename H,typename... T> void _DEBUG(H a,T...b){cerr<<a<<",";_DEBUG(b...);}
+template<class T> istream& operator >>(istream &is, vector<T> &v){for(T &e:v)is>>e;return is;}
+template<class T> ostream& operator <<(ostream &os, vector<T> v){os<<"{";for(T &e:v)os<<e<<(v.size()-(int)(&e-&v[0])>1?", ":"");os<<"}";return os;}
+
+void _DEBUG(){}
+template<typename H,typename... T> void _DEBUG(H a,T...b){cerr<<a<<(sizeof...(b)?",":"\n");_DEBUG(b...);}
 
 inline void in(){}
 template<typename H,typename... T>void in(H &a, T&... b){cin>>a;in(b...);}
 inline void out(){}
-template<typename H,typename... T> void out(H a, T... b){cout<<a<<endl;out(b...);}
+template<typename H,typename... T> void out(H a, T... b){cout<<a<<(sizeof...(b)?" ":"\n");out(b...);}
+
+template<class T> void resz(int n,T& v){v.resize(n);}
+template<class H,class... T> void resz(int n,H& a,T&... b){a.resize(n);resz(n,b...);}
 
 const int INF=1LL<<55;
 const int MOD=1000000007;
